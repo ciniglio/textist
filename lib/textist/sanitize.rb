4130c1b07194d56d4dd08ee34e8cfede3776b1d8
Alejandro Ciniglio <mail@alejandrociniglio.com>
2013-01-13 21:32:26 -0500

    Handles punctutation now.

--- lib/textist/sanitize.rb
+++ lib/textist/sanitize.rb
@@ -2,8 +2,12 @@ require 'sanitize'
 
 module Textist
   module Sanitizer
+    PUNCT = %r{\p{Punct}]}
+
     def sanitize(s)
-      Sanitize.clean(s).downcase
+      s.downcase!
+      s = Sanitize.clean(s)
+      s.gsub! PUNCT, ''
     end
   end
 end
